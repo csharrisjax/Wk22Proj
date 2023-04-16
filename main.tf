6dff3388014f1522b62288aea5b706bc2b72ea01
csharrisjax <119553840+csharrisjax@users.noreply.github.com>
2023-04-16 18:47:49 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,3 @@
-#create the provider
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
-
-# Configure the AWS Provider
-provider "aws" {
-  region = "us-east-1"
-}
-
 # Create a VPC
 resource "aws_vpc" "myvpc1" {
   cidr_block = "10.0.0.0/16"
